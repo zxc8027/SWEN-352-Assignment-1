3a071a4155e01ecc13122e82cc095d8bfe3176b3
zxc8027 <zxc8027@g.rit.edu>
2020-01-24 00:49:54 -0500

    Added Inventory unit tests for getters and toString

--- unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -1,15 +1,53 @@
+/*
+ * Tests the Inventory class.
+ */
+
 package edu.ncsu.csc326.coffeemaker;
 
 import junit.framework.TestCase;
 
 public class InventoryTest extends TestCase {
-
+	private Inventory CuT;
+	
+	/*
+	 * Sets up the test.
+	 */
 	protected void setUp() throws Exception {
-		super.setUp();
+		// Create the inventory. Used by all tests.
+		this.CuT = new Inventory();
+	}
+	
+	/*
+	 * Tests the getChocolate method when unchanged.
+	 */
+	public void testGetChocolateUnchanged() {
+		assertEquals("Default chocolate was incorrect.",15,this.CuT.getChocolate());
+	}
+	
+	/*
+	 * Tests the getCoffee method when unchanged.
+	 */
+	public void testGetCoffeeUnchanged() {
+		assertEquals("Default coffee was incorrect.",15,this.CuT.getCoffee());
+	}
+	
+	/*
+	 * Tests the getMilk method when unchanged.
+	 */
+	public void testGetMilkUnchanged() {
+		assertEquals("Default milk was incorrect.",15,this.CuT.getMilk());
+	}
+	/*
+	 * Tests the getSugar method when unchanged.
+	 */
+	public void testGetSugarUnchanged() {
+		assertEquals("Default sugar was incorrect.",15,this.CuT.getSugar());
 	}
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
+	/*
+	 * Tests the toString method.
+	 */
+	public void testToString() {
+		assertEquals("Inventory as string was incorrect.","Coffee: 15\nMilk: 15\nSugar: 15\nChocolate: 15\n",this.CuT.toString());
 	}
-
 }
