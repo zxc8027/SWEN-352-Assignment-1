7a567ecd4f711b8afd011e3fe3465ebe343de21f
zxc8027 <zxc8027@g.rit.edu>
2020-01-23 19:00:59 -0500

    Initial setup from existing code

---
+++ unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -0,0 +1,15 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import junit.framework.TestCase;
+
+public class InventoryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
