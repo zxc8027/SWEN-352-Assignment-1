32659dabd5db1f1ff1dd336f3301398e6130cf48
zxc8027 <zxc8027@g.rit.edu>
2020-01-24 11:56:17 -0500

    Added addChocolate unit tests

--- unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -4,10 +4,26 @@
 
 package edu.ncsu.csc326.coffeemaker;
 
+import java.util.concurrent.Callable;
+
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
 import junit.framework.TestCase;
 
 public class InventoryTest extends TestCase {
 	private Inventory CuT;
+	
+	/*
+	 * Asserts that an InventoryException was thrown.
+	 */
+	public static void assertInventoryException(Callable method) throws Exception {
+		try {
+			method.call();
+		} catch (InventoryException exception) {
+			
+		} catch (Exception exception) {
+			throw exception;
+		}
+	}
 	
 	/*
 	 * Sets up the test.
@@ -169,5 +185,85 @@ public class InventoryTest extends TestCase {
 		
 		assertEquals("Modified milk was incorrect.",0,this.CuT.getSugar());
 		assertEquals("Inventory as string was incorrect.","Coffee: 15\nMilk: 15\nSugar: 0\nChocolate: 15\n",this.CuT.toString());
+	}
+	
+	/*
+	 * Tests the addChocolate method with an empty string.
+	 */
+	public void testAddChocolateEmptyString() throws Exception {
+		Inventory CuT = this.CuT;
+		assertInventoryException(new Callable() {
+			@Override
+			public Object call() throws Exception {
+				CuT.addChocolate("");
+				return null;
+			}
+		});
+		
+		assertEquals("Chocolate was modified.",15,this.CuT.getChocolate());
+	}
+	
+	/*
+	 * Tests the addChocolate method with a non-number string.
+	 */
+	public void testAddChocolateNonNumberString() throws Exception {
+		Inventory CuT = this.CuT;
+		assertInventoryException(new Callable() {
+			@Override
+			public Object call() throws Exception {
+				CuT.addChocolate("Test");
+				return null;
+			}
+		});
+		
+		assertEquals("Chocolate was modified.",15,this.CuT.getChocolate());
+	}
+	
+	/*
+	 * Tests the addChocolate method with a float string.
+	 */
+	public void testAddChocolateFloat() throws Exception {
+		Inventory CuT = this.CuT;
+		assertInventoryException(new Callable() {
+			@Override
+			public Object call() throws Exception {
+				CuT.addChocolate("3.2");
+				return null;
+			}
+		});
+		
+		assertEquals("Chocolate was modified.",15,this.CuT.getChocolate());
+	}
+	
+	/*
+	 * Tests the addChocolate method with a negative integer string.
+	 */
+	public void testAddChocolateNegativeInteger() throws Exception {
+		Inventory CuT = this.CuT;
+		assertInventoryException(new Callable() {
+			@Override
+			public Object call() throws Exception {
+				CuT.addChocolate("-1");
+				return null;
+			}
+		});
+		
+		assertEquals("Chocolate was modified.",15,this.CuT.getChocolate());
+	}
+	
+	/*
+	 * Tests the addChocolate method with a zero string.
+	 */
+	public void testAddChocolatZero() throws Exception {
+		CuT.addChocolate("0");
+		assertEquals("Chocolate was modified.",15,this.CuT.getChocolate());
+	}
+
+	/*
+	 * Tests the addChocolate method with a positive integer string.
+	 */
+	public void testAddChocolatePositiveInteger() throws Exception {
+		CuT.addChocolate("1");
+		assertEquals("Chocolate wasn't changed correctly.",16,this.CuT.getChocolate());
 	}
 }
