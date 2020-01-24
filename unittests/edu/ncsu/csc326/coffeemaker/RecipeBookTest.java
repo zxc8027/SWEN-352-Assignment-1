460671570cf63f5c940f43b84e7c1997060705a1
Melissa Gould <mag8224@g.rit.edu>
2020-01-24 16:31:06 -0500

    initial test writing

---
+++ unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package edu.ncsu.csc326.coffeemaker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author melissa
+ *
+ */
+class RecipeBookTest {
+    private static CoffeeMaker coffeeMaker;
+    private static Recipe recipe;
+    private static Inventory inventory;
+    private static RecipeBook rb;
+    
+    
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		this.coffeeMaker = new CoffeeMaker();
+	}
+
+	@Test
+	void test_getRecipe() {
+		
+	}
+	
+	@Test
+	void test_addRecipe() {
+		
+	}
+	
+	@Test
+	void test_deleteRecipe() { 
+		
+	}
+	
+	@Test
+	void test_editRecipe() {
+		
+	}
+
+}
