99cccef02deccc4c83fa35475bdc3f52cf741427
lxg8800 <lxg8800@rit.edu>
2020-01-25 14:45:11 -0500

    Remove unreachable code in hashCode method

--- src/edu/ncsu/csc326/coffeemaker/Recipe.java
+++ src/edu/ncsu/csc326/coffeemaker/Recipe.java
@@ -162,7 +162,7 @@ public class Recipe {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + name.hashCode();
 		return result;
 	}
 
