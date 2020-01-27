45440d0555a7c810210f0b1566fb61757e83c641
Melissa Gould <mag8224@g.rit.edu>
2020-01-27 10:53:11 -0500

    fix tests using JUnit3 instead of 5

--- unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
@@ -1,34 +1,14 @@
-/**
- * 
- */
 package edu.ncsu.csc326.coffeemaker;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
-import java.util.concurrent.Callable;
 
 
-/**
- * 
- */
-class RecipeBookTest {
-    private Recipe recipe;
-    private Inventory inventory;
+public class RecipeBookTest extends TestCase {
+	private Recipe recipe;
     private RecipeBook rb;
-    
-    
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception {
+
+	protected void setUp() throws Exception {
+		super.setUp();
 		this.recipe = new Recipe();
 		this.recipe.setName("Test Recipe");
 		this.recipe.setPrice("1");
@@ -38,34 +18,30 @@ class RecipeBookTest {
 		this.recipe.setAmtSugar("2");
 		
 		this.rb = new RecipeBook();
-		this.inventory = new Inventory();
-		
 	}
 
-	@Test
-	void test_getRecipeWithExistingRecipe() {
+	public void test_getRecipeWithExistingRecipe() {
 		this.rb.addRecipe(this.recipe);
 		assertEquals(this.rb.getRecipes().length, 4);
 		assertEquals(this.recipe.getName(), this.rb.getRecipes()[0].getName());
 		assertEquals(this.recipe, this.rb.getRecipes()[0]);
 	}
 	
-	@Test
-	void test_getRecipeWithNoExistingRecipe() {
+	public void test_getRecipeWithNoExistingRecipe() {
 		for( int i = 0; i < this.rb.getRecipes().length; i++) {
 			assertEquals(null, this.rb.getRecipes()[i]);
 		}
 	}
 	
-	@Test
-	void test_addRecipe() {
+	
+	public void test_addRecipe() {
 		this.rb.addRecipe(this.recipe);
 		assertEquals(this.recipe, this.rb.getRecipes()[0]);
 	}
 	
 	
-	@Test
-	void test_addingAllRecipes() throws RecipeException {
+	
+	public void test_addingAllRecipes() throws Exception {
 		
 		this.rb.addRecipe(this.recipe);
 
@@ -80,22 +56,21 @@ class RecipeBookTest {
 
 		
 		Recipe test3 = new Recipe();
-		test2.setName("Test Recipe3");
-		test2.setPrice("3");
-		test2.setAmtChocolate("3");
-		test2.setAmtCoffee("3");
-		test2.setAmtMilk("6");
-		test2.setAmtSugar("6");
-		
+		test3.setName("Test Recipe3");
+		test3.setPrice("3");
+		test3.setAmtChocolate("3");
+		test3.setAmtCoffee("3");
+		test3.setAmtMilk("6");
+		test3.setAmtSugar("6");
 		this.rb.addRecipe(test3);
 
 		Recipe test4 = new Recipe();
-		test2.setName("Test Recipe4");
-		test2.setPrice("4");
-		test2.setAmtChocolate("4");
-		test2.setAmtCoffee("4");
-		test2.setAmtMilk("8");
-		test2.setAmtSugar("8");
+		test4.setName("Test Recipe4");
+		test4.setPrice("4");
+		test4.setAmtChocolate("4");
+		test4.setAmtCoffee("4");
+		test4.setAmtMilk("8");
+		test4.setAmtSugar("8");
 		this.rb.addRecipe(test4);
 		
 		Recipe [] expectedArray = new Recipe[4];
@@ -103,25 +78,23 @@ class RecipeBookTest {
 		expectedArray[1] = test2;
 		expectedArray[2] = test3;
 		expectedArray[3] = test4;
+
 		
 		Recipe [] actualArray = this.rb.getRecipes();
 		for(int i = 0; i < expectedArray.length; i++) {
-			//assertEquals(expectedArray[i], actualArray[i]);
+			assertEquals(expectedArray[i], actualArray[i]);
 		}
 	}
 	
-	
-	
-	@Test
-	void test_addSameRecipe() {
+	public void test_addSameRecipe() {
 		this.rb.addRecipe(this.recipe);
 		assertEquals(this.recipe, this.rb.getRecipes()[0]);
 		this.rb.addRecipe(this.recipe);
 		assertEquals(null, this.rb.getRecipes()[1]);
 	}
 	
-	@Test
-	void test_rolloverRecipe() throws RecipeException {
+	
+	public void test_rolloverRecipe() throws Exception {
 		this.rb.addRecipe(this.recipe);
 
 		Recipe test2 = new Recipe();
@@ -135,22 +108,22 @@ class RecipeBookTest {
 
 		
 		Recipe test3 = new Recipe();
-		test2.setName("Test Recipe3");
-		test2.setPrice("3");
-		test2.setAmtChocolate("3");
-		test2.setAmtCoffee("3");
-		test2.setAmtMilk("6");
-		test2.setAmtSugar("6");
+		test3.setName("Test Recipe3");
+		test3.setPrice("3");
+		test3.setAmtChocolate("3");
+		test3.setAmtCoffee("3");
+		test3.setAmtMilk("6");
+		test3.setAmtSugar("6");
 		
 		this.rb.addRecipe(test3);
 
 		Recipe test4 = new Recipe();
-		test2.setName("Test Recipe4");
-		test2.setPrice("4");
-		test2.setAmtChocolate("4");
-		test2.setAmtCoffee("4");
-		test2.setAmtMilk("8");
-		test2.setAmtSugar("8");
+		test4.setName("Test Recipe4");
+		test4.setPrice("4");
+		test4.setAmtChocolate("4");
+		test4.setAmtCoffee("4");
+		test4.setAmtMilk("8");
+		test4.setAmtSugar("8");
 		this.rb.addRecipe(test4);
 		
 		Recipe test5 = new Recipe();
@@ -163,29 +136,30 @@ class RecipeBookTest {
 		this.rb.addRecipe(test5);
 		
 		Recipe [] expectedArray = new Recipe[4];
-		expectedArray[0] = test5;
+		expectedArray[0] = this.recipe;
 		expectedArray[1] = test2;
 		expectedArray[2] = test3;
 		expectedArray[3] = test4;
 		
-		//assertEquals(test5, this.rb.getRecipes()[0]);
-		
-		
+		Recipe [] actualArray = this.rb.getRecipes();
+		for(int i = 0; i < expectedArray.length; i++) {
+			assertEquals(expectedArray[i], actualArray[i]);
+		}		
 	}
 	
-	@Test
-	void test_addNullRecipe() {
+	
+	public void test_addNullRecipe() {
 		assertEquals(false, this.rb.addRecipe(null));
 	}
 	
-	@Test
-	void test_addBlankRecipe() {
+	
+	public void test_addBlankRecipe() {
 		assertEquals(true, this.rb.addRecipe(new Recipe()));
 	}
 	
 	
-	@Test
-	void test_deleteRecipe() { 
+	
+	public void test_deleteRecipe() { 
 		this.rb.addRecipe(this.recipe);
 		assertEquals("Test Recipe", this.rb.deleteRecipe(0));
 		Recipe [] recipes = this.rb.getRecipes();
@@ -193,24 +167,23 @@ class RecipeBookTest {
 	}
 	
 
-	@Test
-	void test_deleteNoRecipesExist() {
+	
+	public void test_deleteNoRecipesExist() {
 		assertEquals(null, this.rb.deleteRecipe(0));
 	}
 	
-	@Test
-	void test_editRecipe() throws RecipeException {
+	
+	public void test_editRecipe() throws Exception {
 		assertEquals(true, this.rb.addRecipe(this.recipe));
 		this.recipe.setAmtChocolate("10");
 		assertEquals("Test Recipe", this.rb.editRecipe(0, this.recipe));
 		assertEquals(this.recipe.getName(), this.rb.getRecipes()[0].getName());
 	}
 	
-	@Test
-	void test_editNullRecipe() {
+	
+	public void test_editNullRecipe() {
 		assertEquals(null, this.rb.editRecipe(0, this.recipe));
 	}
-	
-	
+
 
 }
